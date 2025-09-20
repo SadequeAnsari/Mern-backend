4a72ebfb1900ad120c05e8dab6bd7db9aad3d61e
Mohammad Sadeque Ansari <sadequeansari7292@gmail.com>
2025-09-20 22:42:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ connectDB();
 const allowedOrigins = ['https://mern-frontend-eta-self.vercel.app'];
 app.use(cors({
   origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
+  },
+  credentials: true // Add this line
 }));
 
 app.use(express.json());
