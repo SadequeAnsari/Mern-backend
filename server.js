42083962bb101947fd6db783db032f3af04cd348
Mohammad Sadeque Ansari <sadequeansari7292@gmail.com>
2025-09-20 13:40:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,24 +13,24 @@ const { otps, verificationCodes } = require("./utils/inMemoryStore");
 connectDB();
 
 // CORS setup
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN,
-//   credentials: true
-// }));
-
-
-const allowedOrigins = ['https://mern-frontend-eta-self.vercel.app', , 'http://localhost:5173'];
 app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
 }));
+
+
+// const allowedOrigins = ['https://mern-frontend-eta-self.vercel.app', , 'http://localhost:5173'];
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
